209d03f539ed08156a65f1d9679ae9771803daeb
Sugaraa1 <iempty023@gmail.com>
2025-11-21 21:53:02 +0800

    suuld zassan

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -1,56 +1,20 @@
 <?php 
-
-
-
     require('../admin/inc/db_config.php');
     require('../admin/inc/essentials.php');
-    require("../inc/sendgrid/sendgrid-php.php");
-
-
-    function send_mail($uemail,$name,$token)
-    {
-        $email = new \SendGrid\Mail\Mail(); 
-        $email->setFrom("iempty023@gmail.com", "Meegii");
-        $email->setSubject("ACC verification");
-        $email->addTo($email,$name);
-
-        $email->addContent(
-            "text/html", 
-            "
-                Click the link <br>
-                <a href='".SITE_URL."email_confirm.php?email=$uemail&token=$token"."'>
-                CLICK ME
-                </a>
-            "
-        );
-        $sendgrid = new \SendGrid(SENDGRID_API_KEY);
-        try{
-            $sendgrid->send($email);
-                return 1;
-            }
-            catch (Exception $e){
-                return 0;
-            }
-        }
-         
-
-
-    
 
     if(isset($_POST['register']))
     {
         $data = filteration($_POST);
 
-        //matching passwords
-
+        // Нууц үг тохирч байгаа эсэхийг шалгах
         if($data['pass'] != $data['cpass']){
             echo 'pass_mismatch';
             exit;
         }
 
-        // check user exist or not
-
-        $u_exist = select("SELECT * FROM `user_cred` WHERE `email` = ? OR  `phonenum` = ? LIMIT 1", [$data['email'],$data['phonenum']], "ss");
+        // Хэрэглэгч өмнө нь бүртгэлтэй эсэхийг шалгах
+        $u_exist = select("SELECT * FROM `user_cred` WHERE `email` = ? OR `phonenum` = ? LIMIT 1", 
+                         [$data['email'],$data['phonenum']], "ss");
 
         if(mysqli_num_rows($u_exist)!=0){
             $u_exist_fetch = mysqli_fetch_assoc($u_exist);
@@ -58,8 +22,7 @@
             exit;
         }
 
-        // upload user image to server
-
+        // Хэрэглэгчийн зургийг хадгалах
         $img = uploadUserImage($_FILES['profile']);
 
         if($img == 'inv_img'){
@@ -71,31 +34,54 @@
             exit; 
         }
 
-        //send confirmation email
-
-        $token = bin2hex(random_bytes(16));
-
-        if(!send_mail($data['email'],$data['name'],$token))
-        {
-            echo 'mail_failed';
-            exit;
-        }
-
-
+        // Нууц үгийг hash хийх
         $enc_pass = password_hash($data['pass'], PASSWORD_BCRYPT);
 
-        $query = "INSERT INTO `user_cred`(`name`, `email`, `address`, `phonenum`, `pincode`, `dob`, `profile`, `password`, `token`) VALUES (?,?,?,?,?,?,?,?,?)";
+        // is_verified = 1 болгож, token = NULL болгож өгч байна (email баталгаажуулалт хэрэггүй)
+        $query = "INSERT INTO `user_cred`(`name`, `email`, `address`, `phonenum`, `pincode`, `dob`, `profile`, `password`, `is_verified`) 
+                  VALUES (?,?,?,?,?,?,?,?,?)";
 
-        $values = [$data['name'],$data['email'],$data['address'],$data['phonenum'],$data['pincode'],
-        $data['dob'],$img,$enc_pass,$token];
+        $values = [$data['name'], $data['email'], $data['address'], $data['phonenum'], 
+                   $data['pincode'], $data['dob'], $img, $enc_pass, 1]; // is_verified = 1
 
-        if(insert($query,$values,'sssssssss'))
+        if(insert($query, $values, 'ssssssssi'))
         {
-            echo 1;
+            echo 1; // Амжилттай бүртгэгдлээ
         }
         else{
             echo 'ins_failed';
         }
+    }
 
+    if(isset($_POST['login']))
+    {
+        $data = filteration($_POST);
+
+        // Email эсвэл утасны дугаар болон нууц үгээр нэвтрэх
+        $u_exist = select("SELECT * FROM `user_cred` WHERE `email`=? OR `phonenum`=? LIMIT 1",
+                         [$data['email_or_phone'], $data['email_or_phone']], "ss");
+
+        if(mysqli_num_rows($u_exist) == 0){
+            echo 'inv_email_mob'; // Хэрэглэгч олдсонгүй
+            exit;
+        }
+
+        $u_fetch = mysqli_fetch_assoc($u_exist);
+
+        // Нууц үг тохирч байгаа эсэхийг шалгах
+        if(!password_verify($data['pass'], $u_fetch['password'])){
+            echo 'invalid_pass'; // Нууц үг буруу
+            exit;
+        }
+
+        // Session эхлүүлэх
+        session_start();
+        $_SESSION['login'] = true;
+        $_SESSION['uId'] = $u_fetch['id'];
+        $_SESSION['uName'] = $u_fetch['name'];
+        $_SESSION['uPic'] = $u_fetch['profile'];
+        $_SESSION['uPhone'] = $u_fetch['phonenum'];
+
+        echo 1; // Амжилттай нэвтэрлээ
     }
 ?>
--- inc/footer.php
+++ inc/footer.php
@@ -1,148 +1,168 @@
 <div class="container-fluid bg-white mt-5">
-        <div class="row">
-            <div class="col-lg-4 p-4">
-                <h3 class="h-font fw-bold fs-3 mb-2">METRO HOTEL</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id placeat, itaque aliquid voluptatem, alias assumenda enim at reiciendis odio, consequuntur sunt saepe fugit architecto beatae ab iusto. Obcaecati, cupiditate magnam!</p>
-            </div>
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Links</h5>
-                <a href="index.php" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
-                <a href="rooms.php" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
-                <a href="facilities.php" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
-                <a href="contact.php" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a> <br>
-                <a href="about.php" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
-            </div> 
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Follow Us</h5>
-                <?php
-                    if($contact_r['tw']!=''){
-                        echo<<<data
-                        <a href="{$contact_r['tw']}" class="d-inline-block text-dark text-decoration-none mb-2">
-                            <i class="bi bi-twitter me-1"></i> Twitter
-                        </a><br>
-                        data;
-                    }
-                ?>
-                <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block text-dark text-decoration-none mb-2">
-                    <i class="bi bi-facebook me-1"></i> Facebook
-                </a><br>
-                <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block text-dark text-decoration-none">
-                    <i class="bi bi-instagram me-1"></i> Instagram
-                </a><br>
-            </div>
+    <div class="row">
+        <div class="col-lg-4 p-4">
+            <h3 class="h-font fw-bold fs-3 mb-2">METRO HOTEL</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id placeat, itaque aliquid voluptatem, alias assumenda enim at reiciendis odio, consequuntur sunt saepe fugit architecto beatae ab iusto. Obcaecati, cupiditate magnam!</p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="index.php" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+            <a href="rooms.php" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+            <a href="facilities.php" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+            <a href="contact.php" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a> <br>
+            <a href="about.php" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+        </div> 
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow Us</h5>
+            <?php
+                if($contact_r['tw']!=''){
+                    echo<<<data
+                    <a href="{$contact_r['tw']}" class="d-inline-block text-dark text-decoration-none mb-2">
+                        <i class="bi bi-twitter me-1"></i> Twitter
+                    </a><br>
+                    data;
+                }
+            ?>
+            <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-facebook me-1"></i> Facebook
+            </a><br>
+            <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block text-dark text-decoration-none">
+                <i class="bi bi-instagram me-1"></i> Instagram
+            </a><br>
         </div>
     </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <script>
-
-    function alert(type,msg,position='body')
-    {
+<script>
+    function alert(type, msg, position='body') {
         let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
         let element = document.createElement('div');
-        element.innerHTML = `<div class="alert ${bs_class}       alert-dismissible fade show" role="alert">
+        element.innerHTML = `<div class="alert ${bs_class} alert-dismissible fade show" role="alert">
             <strong class="me-2">${msg}</strong> 
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        `;
+        </div>`;
 
         if(position=='body'){
             document.body.append(element);
             element.classList.add('custom-alert');
-        }
-        else{
+        } else {
             document.getElementById(position).appendChild(element);
         }
         setTimeout(remAlert, 3000);
-        }
+    }
 
-        function remAlert(){
+    function remAlert(){
         document.getElementsByClassName('alert')[0].remove();
     }
 
+    function setActive() {
+        let navbar = document.getElementById('nav-bar');
+        let a_tags = navbar.getElementsByTagName('a');
 
+        for(i=0; i<a_tags.length; i++) {
+            let file = a_tags[i].href.split('/').pop();
+            let file_name = file.split('.')[0];
 
-        function setActive()
-        {
-            let navbar = document.getElementById('nav-bar');
-            let a_tags = navbar.getElementsByTagName('a');
+            if(document.location.href.indexOf(file_name) >= 0){
+                a_tags[i].classList.add('active');
+            }
+        }
+    }
 
-            for(i=0; i<a_tags.length; i++)
-            {
-                let file = a_tags[i].href.split('/').pop();
-                let file_name = file.split('.')[0];
+    // Register form
+    let register_form = document.getElementById('register-form');
 
-                if(document.location.href.indexOf(file_name) >= 0){
-                    a_tags[i].classList.add('active');
-                }
+    register_form.addEventListener('submit', (e)=>{
+        e.preventDefault();
 
+        let data = new FormData();
+        
+        data.append('name', register_form.elements['name'].value);
+        data.append('email', register_form.elements['email'].value);
+        data.append('phonenum', register_form.elements['phonenum'].value);      
+        data.append('address', register_form.elements['address'].value);
+        data.append('pincode', register_form.elements['pincode'].value);
+        data.append('dob', register_form.elements['dob'].value);
+        data.append('pass', register_form.elements['pass'].value);
+        data.append('cpass', register_form.elements['cpass'].value); 
+        data.append('profile', register_form.elements['profile'].files[0]);
+        data.append('register', '');
+
+        var myModal = document.getElementById('registerModal');
+        var modal = bootstrap.Modal.getInstance(myModal);
+        modal.hide();
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "ajax/login_register.php", true);
+
+        xhr.onload = function(){
+            if(this.responseText == 'pass_mismatch'){
+                alert('error', "Нууц үг таарахгүй байна!");
+            }
+            else if(this.responseText == 'email_already'){
+                alert('error', "Email бүртгэлтэй байна!");
+            }
+            else if(this.responseText == 'phone_already'){
+                alert('error', "Утасны дугаар бүртгэлтэй байна!");
+            }
+            else if(this.responseText == 'inv_img'){
+                alert('error', "Зөвхөн JPG, JPEG, PNG зураг зөвшөөрнө!");
+            }
+            else if(this.responseText == 'upd_failed'){
+                alert('error', "Зураг хуулахад алдаа гарлаа!");
+            }
+            else if(this.responseText == 'ins_failed'){
+                alert('error', "Бүртгэл амжилтгүй боллоо!");
+            }
+            else {
+                alert('success', "Амжилттай бүртгэгдлээ! Та нэвтэрч орно уу.");
+                register_form.reset();
+            }             
+        }
+
+        xhr.send(data);
+    });
+
+    // Login form
+    let login_form = document.getElementById('login-form');
+
+    login_form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+
+        let data = new FormData();
+        data.append('email_or_phone', login_form.elements['email_or_phone'].value);
+        data.append('pass', login_form.elements['pass'].value);
+        data.append('login', '');
+
+        var myModal = document.getElementById('loginModal');
+        var modal = bootstrap.Modal.getInstance(myModal);
+        modal.hide();
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "ajax/login_register.php", true);
+
+        xhr.onload = function(){
+            if(this.responseText == 'inv_email_mob'){
+                alert('error', "Email эсвэл утасны дугаар олдсонгүй!");
+            }
+            else if(this.responseText == 'invalid_pass'){
+                alert('error', "Нууц үг буруу байна!");
+            }
+            else if(this.responseText == '1'){
+                alert('success', "Амжилттай нэвтэрлээ!");
+                setTimeout(function(){
+                    window.location.href = 'index.php';
+                }, 1500);
+            }
+            else {
+                alert('error', "Алдаа гарлаа!");
             }
         }
 
+        xhr.send(data);
+    });
 
-
-
-        let register_form = document.getElementById('register-form');
-
-        register_form.addEventListener('submit', (e)=>{
-            e.preventDefault();
-
-            let data = new FormData();
-            
-            data.append('name',register_form.elements['name'].value);
-            data.append('email',register_form.elements['email'].value);
-            data.append('phonenum',register_form.elements['phonenum'].value);      
-            data.append('address',register_form.elements['address'].value);
-            data.append('pincode',register_form.elements['pincode'].value);
-            data.append('dob',register_form.elements['dob'].value);
-            data.append('pass',register_form.elements['pass'].value);
-            data.append('cpass',register_form.elements['cpass'].value); 
-            data.append('profile',register_form.elements['profile'].files[0]);
-            data.append('register','');
-
-            var myModal = document.getElementById('registerModal');
-            var modal = bootstrap.Modal.getInstance(myModal);
-            modal.hide();
-
-            let xhr = new XMLHttpRequest();
-            // xhr.open("POST","ajax/login_register.php",true);
-            xhr.open("POST","/metroHotel/ajax/login_register.php", true);
-
-
-            xhr.onload = function(){
-              if(this.responseText == 'pass_mismatch'){
-                alert('error',"Password Mismatch!");
-              }
-              else if(this.responseText == 'email_already'){
-                alert('error',"Email Already Registered!");
-              }
-              else if(this.responseText == 'phone_already'){
-                alert('error',"PhoneNumber Already Registered!");
-              }
-              else if(this.responseText == 'inv_img'){
-                alert('error',"Only JPG, JPEG, PNG images are allowed!");
-              }
-              else if(this.responseText == 'upd_failed'){
-                alert('error',"Image Upload Failed!");
-              }
-              else if(this.responseText == 'mail_failed'){
-                alert('error',"Cannot send confirmation email! Server down!");
-              }
-              else if(this.responseText == 'ins_failed'){
-                alert('error',"Registration Failed! Server Down!");
-              }
-              else{
-                alert('success',"Registration Succesful! Confirmation link sent to email.");
-                register_form.reset();
-              }             
-            }
-
-
-            xhr.send(data);
-        });
-
-
-
-        setActive();
-    </script>
+    setActive();
+</script>
